0d24802c5a4e411dd53f61c50dd09452744e20f9
Viliam <vatvil@gmail.com>
2024-07-22 21:08:21 +0200

    init project

---
+++ app.js
@@ -0,0 +1,92 @@
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const gravity = 0.5;
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
+    this.velocity = {
+      x: 0,
+      y: 0,
+    };
+    this.width = 30;
+    this.height = 30;
+  }
+
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update() {
+    this.draw();
+    this.position.y += this.velocity.y;
+    this.position.x += this.velocity.x;
+
+    this.velocity.y += gravity;
+
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
+  }
+}
+
+const player = new Player();
+player.update();
+
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  player.update();
+}
+
+animate();
+window.addEventListener("keydown", ({ keyCode }) => {
+  switch (keyCode) {
+    case 65:
+      console.log("left");
+      break;
+    case 87:
+      console.log("up");
+      player.velocity.y = -20;
+      break;
+    case 68:
+      console.log("right");
+      player.velocity.x = 5;
+      break;
+    case 83:
+      console.log("down");
+      break;
+    default:
+      break;
+  }
+});
+
+window.addEventListener("keyup", ({ keyCode }) => {
+  switch (keyCode) {
+    case 65:
+      console.log("left");
+      break;
+    case 87:
+      console.log("up");
+      player.velocity.y = -20;
+      break;
+    case 68:
+      console.log("right");
+      player.velocity.x = 0;
+      break;
+    case 83:
+      console.log("down");
+      break;
+    default:
+      break;
+  }
+});
